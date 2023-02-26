b20e5c0b09b7126b0bb6b444f27b7c9440fdc93f
NuoWenLei <nuowen0612@gmail.com>
2023-02-25 23:38:37 -0500

    adjust data loader and trainer

--- data_loader.py
+++ data_loader.py
@@ -15,25 +15,28 @@ class DataLoader():
 	      batchSize: int = 32,
 	      returnDataset: bool = False,
 		  shuffle: bool = True,
-		  bucketColumn: str = None):
+		  bucketColumn: str = None,
+		  numBatch: int = None):
 		self.sampleCsv = pd.read_csv(csvPath)
 		self.targetColumn = targetColumn
 		self.samplingRate = samplingRate
 		self.numSamples = numSamples
 		self.returnDataset = returnDataset
 		self.batchSize = batchSize
+		self.epoch = 0
 		self.batch = 0
 		self.audioColumn = audioColumn
 		self.newAudioColumn = newAudioColumn
 		self.audioDirPath = audioDirPath
-		if bucketColumn is None:
+		if (bucketColumn is None):
 			self.bucketColumn = "bucket"
 			self.bucketize(shuffle = shuffle)
 		else:
-			if (bucketColumn in self.sampleCsv.columns):
+			if (bucketColumn in self.sampleCsv.columns) and (numBatch is not None):
 				self.bucketColumn = bucketColumn
+				self.numBatch = numBatch
 			else:
-				raise Exception(f"Bucket column {bucketColumn} does not exist")
+				raise Exception(f"Bucket column {bucketColumn} does not exist or Number of batches (numBatch) not declared!!!")
 		if (not self.returnDataset) and (self.targetColumn is None):
 			raise Exception("Must have target column if not returning entire dataset")
 
@@ -41,8 +44,14 @@ class DataLoader():
 		if shuffle:
 			self.sampleCsv = self.sampleCsv.sample(frac = 1).reset_index()
 		self.sampleCsv["bucket"] = (self.sampleCsv.index.values // self.batchSize)
+		self.numBatch = self.sampleCsv["bucket"].max()
 	
 	def nextBatch(self):
+		
+		if self.batch == self.numBatch:
+			self.batch = 0
+			self.epoch += 1
+		
 		ds = Dataset.from_pandas(self.sampleCsv[self.sampleCsv[self.bucketColumn] == self.batch])\
 			.cast_column(self.audioColumn, Audio(sampling_rate=self.samplingRate))
 		
@@ -52,9 +61,11 @@ class DataLoader():
 		
 		ds = ds.map(cropAudioAndCreateNewColumn)
 
+		self.batch += 1
+
 		if self.returnDataset:
 			return ds
 		else:
 			ds = ds.with_format("np")
-			return (ds[self.newAudioColumn], ds[self.targetColumn])
+			return (np.asarray(ds[self.newAudioColumn]), np.asarray(ds[self.targetColumn]))
 		
--- trainer.py
+++ trainer.py
@@ -8,154 +8,140 @@ import os
 from tqdm import tqdm
 import wandb
 from audio_diffusion_pytorch import DiffusionModel, UNetV0, VDiffusion, VSampler
-from dataset import load_data, load_dataset
+from dataset import load_data
 from pathlib import Path
+from data_loader import DataLoader
 
-SAMPLE_RATE = 16000
+SAMPLE_RATE = 30000
 BATCH_SIZE = 12
 NUM_SAMPLES = 2**18
 
 
 def create_model():
-    return DiffusionModel(
-        net_t=UNetV0, # The model type used for diffusion (U-Net V0 in this case)
-        in_channels=1, # U-Net: number of input/output (audio) channels
-        channels=[8, 32, 64, 128, 256, 512, 512, 1024, 1024], # U-Net: channels at each layer
-        factors=[1, 4, 4, 4, 2, 2, 2, 2, 2], # U-Net: downsampling and upsampling factors at each layer
-        items=[1, 2, 2, 2, 2, 2, 2, 4, 4], # U-Net: number of repeating items at each layer
-        attentions=[0, 0, 0, 0, 0, 1, 1, 1, 1], # U-Net: attention enabled/disabled at each layer
-        attention_heads=8, # U-Net: number of attention heads per attention item
-        attention_features=64, # U-Net: number of attention features per attention item
-        diffusion_t=VDiffusion, # The diffusion method used
-        sampler_t=VSampler, # The diffusion sampler used
-    )
+	return DiffusionModel(
+		net_t=UNetV0, # The model type used for diffusion (U-Net V0 in this case)
+		in_channels=1, # U-Net: number of input/output (audio) channels
+		channels=[8, 32, 64, 128, 256, 512, 512, 1024, 1024], # U-Net: channels at each layer
+		factors=[1, 4, 4, 4, 2, 2, 2, 2, 2], # U-Net: downsampling and upsampling factors at each layer
+		items=[1, 2, 2, 2, 2, 2, 2, 4, 4], # U-Net: number of repeating items at each layer
+		attentions=[0, 0, 0, 0, 0, 1, 1, 1, 1], # U-Net: attention enabled/disabled at each layer
+		attention_heads=8, # U-Net: number of attention heads per attention item
+		attention_features=64, # U-Net: number of attention features per attention item
+		diffusion_t=VDiffusion, # The diffusion method used
+		sampler_t=VSampler, # The diffusion sampler used
+	)
 
 
 def main():
-    args = parse_args()
+	args = {
+		# DataLoader args
+		"csvPath": "TODO",
+		"audioDirPath": "TODO",
+		"audioColumn": "TODO",
+		"targetColumn": "TODO",
+		"samplingRate": SAMPLE_RATE,
+		"numSamples": NUM_SAMPLES,
+		"batchSize": BATCH_SIZE,
+		"returnDataset": False,
+		"shuffle": True,
+	}
 
-    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    print(f"Using device: {device}")
+	device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+	print(f"Using device: {device}")
 
-    dataset = load_data()
-    data_dir = "/Users/nuowenlei/Documents/GitHub/AudioDiffusionTrainer/music_data" # Where to save the data
-    # Create where data is stored
-    dir = Path(data_dir)
-    dir.mkdir(exist_ok=True, parents=True)
+	dataset = load_data()
+	data_dir = "/Users/nuowenlei/Documents/GitHub/AudioDiffusionTrainer/music_data" # Where to save the data
+	# Create where data is stored
+	dir = Path(data_dir)
+	dir.mkdir(exist_ok=True, parents=True)
 
-    print(f"Dataset length: {len(dataset)}")
+	print(f"Dataset length: {len(dataset)}")
 
-    torchaudio.save("test.wav", dataset[0], SAMPLE_RATE)
+	torchaudio.save("test.wav", dataset[0], SAMPLE_RATE)
 
-    # dataloader = torch.utils.data.DataLoader(
-    #     dataset,
-    #     batch_size=BATCH_SIZE,
-    #     shuffle=True,
-    #     num_workers=0,
-    #     pin_memory=True,
-    # )
+	dataloader = DataLoader(
+		csvPath=args["csvPath"],
+		audioDirPath=args["audioDirPath"],
+		audioColumn=args["audioColumn"],
+		targetColumn=args["targetColumn"],
+		samplingRate=args["samplingRate"],
+		numSamples=args["numSamples"],
+		batchSize=args["batchSize"],
+		returnDataset=args["returnDataset"],
+		shuffle=args["shuffle"]
+	)
 
-    model = create_model().to(device)
+	model = create_model().to(device)
 
-    optimizer = torch.optim.Adam(model.parameters(), lr=1e-4)
+	optimizer = torch.optim.Adam(model.parameters(), lr=1e-4)
 
-    print(f"Number of parameters: {sum(p.numel() for p in model.parameters() if p.requires_grad)}")
+	print(f"Number of parameters: {sum(p.numel() for p in model.parameters() if p.requires_grad)}")
 
-    run_id = wandb.util.generate_id()
-    if args.run_id is not None:
-        run_id = args.run_id
-    print(f"Run ID: {run_id}")
+	epoch = 0
+	step = 0
+	
+	scaler = torch.cuda.amp.GradScaler()
 
-    wandb.init(project="audio-diffusion", resume=args.resume, id=run_id)
+	checkpoint_path = "checkpoint-audio-diffusion.pt"
 
-    epoch = 0
-    step = 0
+	model.train()
+	while epoch < 100:
+		avg_loss = 0
+		avg_loss_step = 0
+		progress = tqdm(range(dataloader.numBatch))
+		for i in progress:
+			audio, caption = dataloader.nextBatch()
+			with torch.autograd.set_detect_anomaly(True):
+				optimizer.zero_grad()
+				audio = audio.to(device)
+				with torch.cuda.amp.autocast():
+					loss = model(audio,
+		  				text = caption.tolist()
+		  			)
+					avg_loss += loss.item()
+					avg_loss_step += 1
+				scaler.scale(loss).backward()
+				scaler.step(optimizer)
+				scaler.update()
+				progress.set_postfix(
+					loss=loss.item(),
+					epoch=epoch + i / dataloader.numBatch,
+				)
 
-    if args.checkpoint is not None:
-        checkpoint_path = args.checkpoint
-    else:
-        checkpoint_path = f"checkpoint-{run_id}.pt"
+				if step % 200 == 0:
+					# Turn noise into new audio sample with diffusion
+					noise = torch.randn(1, 1, NUM_SAMPLES, device=device)
+					with torch.cuda.amp.autocast():
+						sample = model.sample(noise, num_steps=100)
 
-    if wandb.run.resumed:
-        if os.path.exists(checkpoint_path):
-            checkpoint = torch.load(checkpoint_path)
-        else:
-            checkpoint = torch.load(wandb.restore(checkpoint_path))
-        model.load_state_dict(checkpoint['model_state_dict'])
-        optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
-        epoch = checkpoint['epoch']
-        step = epoch * len(dataloader)
-    
-    scaler = torch.cuda.amp.GradScaler()
+					torchaudio.save(f'test_generated_sound_{step}.wav', sample[0].cpu(), SAMPLE_RATE)
+					del sample
+					gc.collect()
+					torch.cuda.empty_cache()
+				
+				if step % 100 == 0:
+					print(f"Step {step}, Epoch {epoch + i / dataloader.numBatch}, loss {avg_loss / avg_loss_step}")
+					avg_loss = 0
+					avg_loss_step = 0
+				
+				step += 1
 
-    model.train()
-    while epoch < 100:
-        avg_loss = 0
-        avg_loss_step = 0
-        progress = tqdm(dataloader)
-        for i, audio in enumerate(progress):
-            with torch.autograd.set_detect_anomaly(True):
-                optimizer.zero_grad()
-                audio = audio.to(device)
-                with torch.cuda.amp.autocast():
-                    loss = model(audio
-                                 
-                        )
-                    avg_loss += loss.item()
-                    avg_loss_step += 1
-                scaler.scale(loss).backward()
-                scaler.step(optimizer)
-                scaler.update()
-                progress.set_postfix(
-                    loss=loss.item(),
-                    epoch=epoch + i / len(dataloader),
-                )
-
-                if step % 500 == 0:
-                    # Turn noise into new audio sample with diffusion
-                    noise = torch.randn(1, 1, NUM_SAMPLES, device=device)
-                    with torch.cuda.amp.autocast():
-                        sample = model.sample(noise, num_steps=100)
-
-                    torchaudio.save(f'test_generated_sound_{step}.wav', sample[0].cpu(), SAMPLE_RATE)
-                    del sample
-                    gc.collect()
-                    torch.cuda.empty_cache()
-
-                    wandb.log({
-                        "step": step,
-                        "epoch": epoch + i / len(dataloader),
-                        "loss": avg_loss / avg_loss_step,
-                        "generated_audio": wandb.Audio(f'test_generated_sound_{step}.wav', caption="Generated audio", sample_rate=SAMPLE_RATE),
-                    })
-                
-                if step % 100 == 0:
-                    wandb.log({
-                        "step": step,
-                        "epoch": epoch + i / len(dataloader),
-                        "loss": avg_loss / avg_loss_step,
-                    })
-                    avg_loss = 0
-                    avg_loss_step = 0
-                
-                step += 1
-
-        epoch += 1
-        torch.save({
-            'epoch': epoch,
-            'model_state_dict': model.state_dict(),
-            'optimizer_state_dict': optimizer.state_dict(),
-        }, checkpoint_path)
-        wandb.save(checkpoint_path)
+		epoch += 1
+		torch.save({
+			'epoch': epoch,
+			'model_state_dict': model.state_dict(),
+			'optimizer_state_dict': optimizer.state_dict(),
+		}, checkpoint_path)
+		wandb.save(checkpoint_path)
 
 
 def parse_args():
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--checkpoint", type=str, default=None)
-    parser.add_argument("--resume", action="store_true")
-    parser.add_argument("--run_id", type=str, default=None)
-    return parser.parse_args()
+	parser = argparse.ArgumentParser()
+	parser.add_argument("--checkpoint", type=str, default=None)
+	parser.add_argument("--resume", action="store_true")
+	parser.add_argument("--run_id", type=str, default=None)
+	return parser.parse_args()
 
 
 if __name__ == "__main__":
-    main()
+	main()
