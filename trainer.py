a89f0d8c079749b16827f15ad69d86a79d48b416
NuoWenLei <nuowen0612@gmail.com>
2023-03-03 15:04:37 -0500

    Update trainer.py

--- trainer.py
+++ trainer.py
@@ -36,12 +36,12 @@ def create_model(text_condition = False):
 		return DiffusionModel(
 			net_t=UNetV0, # The model type used for diffusion (U-Net V0 in this case)
 			in_channels=1, # U-Net: number of input/output (audio) channels
-			channels=[8, 32, 64, 128, 256, 512, 512, 1024, 1024], # U-Net: channels at each layer
-			factors=[1, 4, 4, 4, 2, 2, 2, 2, 2], # U-Net: downsampling and upsampling factors at each layer
-			items=[1, 2, 2, 2, 2, 2, 2, 4, 4], # U-Net: number of repeating items at each layer
-			attentions=[0, 0, 0, 0, 0, 1, 1, 1, 1], # U-Net: attention enabled/disabled at each layer
-			attention_heads=8, # U-Net: number of attention heads per attention item
-			attention_features=64, # U-Net: number of attention features per attention item
+			channels=[8, 32, 64, 128, 256], # U-Net: channels at each layer
+			factors=[1, 4, 4, 2, 2], # U-Net: downsampling and upsampling factors at each layer
+			items=[1, 2, 2, 2, 4], # U-Net: number of repeating items at each layer
+			attentions=[0, 0, 0, 1, 1], # U-Net: attention enabled/disabled at each layer
+			attention_heads=4, # U-Net: number of attention heads per attention item
+			attention_features=32, # U-Net: number of attention features per attention item
 			diffusion_t=VDiffusion, # The diffusion method used
 			sampler_t=VSampler, # The diffusion sampler used
 		)
